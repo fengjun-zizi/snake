5761f5051ec05dcb1649f602bb9d34fbfc908956
李沣峻 <lifengjun@lifengjundeMac-mini.local>
2025-05-28 17:39:31 +0700

    feat:add some new feature

--- snake_demo.py
+++ snake_demo.py
@@ -23,4 +23,10 @@ class MyGame():
         self.game_name = kwargs.get("game_name") or GAME_NAME
         pygame.display.set_caption(self.game_name)
         self.screen_size = kwargs.get("screen_size") or SCREEN_SIZE
-        
+        self.screen_width , self.screen_height = self.screen_size
+        self.display_mode = kwargs.get("display_mode") or DISPLAY_MODE
+        self.images = {}
+        self.souds = {}
+        self.musics = {}
+        self.icon = kwargs.get("icon") or None
+        self.icon and pygame.display.set_icon(pygame.image)
